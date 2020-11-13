7ec9a9b8bd9fc315a49fe98eb277f97255c6c8c2
Jordan Lancheros <jordan1703@hotmail.es>
2020-11-13 18:44:21 -0500

    change max rsi

--- src/modules/strategy/strategies/trader_macd.js
+++ src/modules/strategy/strategies/trader_macd.js
@@ -721,14 +721,14 @@ module.exports = class TraderCustom {
 
     //RSI 6H, 4H, 1H
 
-    let resolve_rsi = this.resolve_rsi(debug, rsi6H, count_rsi6H, 30, 70);
+    let resolve_rsi = this.resolve_rsi(debug, rsi6H, count_rsi6H, 20, 80);
     count_signals_buy += resolve_rsi.buy;
     count_signals_sell += resolve_rsi.sell;
     debug.rsi6H += resolve_rsi.buy;
     debug.rsi6H -= resolve_rsi.sell;
     debug = resolve_rsi.debug;
 
-    resolve_rsi = this.resolve_rsi(debug, rsi4H, count_rsi4H, 30, 70);
+    resolve_rsi = this.resolve_rsi(debug, rsi4H, count_rsi4H, 20, 80);
     count_signals_buy += resolve_rsi.buy;
     count_signals_sell += resolve_rsi.sell;
     debug.rsi4H += resolve_rsi.buy;
