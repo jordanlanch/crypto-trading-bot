3c549dd3b6f0a33a1edd473feba4346daffb3e15
Jordan Lancheros <jordan1703@hotmail.es>
2020-12-08 21:07:58 -0500

    add to trading

--- src/modules/listener/tick_listener.js
+++ src/modules/listener/tick_listener.js
@@ -220,8 +220,8 @@ module.exports = class TickListener {
                 */
                 console.log('type.name-->' + type.name);
                 if (type.name === 'watch') {
-                  await me.visitStrategy(strategy, symbol);
-                  //await me.visitTradeStrategy(strategy, symbol);
+                  //await me.visitStrategy(strategy, symbol);
+                  await me.visitTradeStrategy(strategy, symbol);
                 } else if (type.name === 'trade') {
                   await me.visitTradeStrategy(strategy, symbol);
                 } else {
