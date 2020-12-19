f6574387af0ca1a95a56233e8885e753a0040f1e
Jordan Lancheros <jordan1703@hotmail.es>
2020-12-18 19:31:35 -0500

    update strategies

--- src/modules/listener/tick_listener.js
+++ src/modules/listener/tick_listener.js
@@ -220,10 +220,10 @@ module.exports = class TickListener {
                 */
                 console.log('type.name-->' + type.name);
                 if (type.name === 'watch') {
-                  //await me.visitStrategy(strategy, symbol);
-                  await me.visitTradeStrategy(strategy, symbol);
+                  await me.visitStrategy(strategy, symbol);
+                  //await me.visitTradeStrategy(strategy, symbol);
                 } else if (type.name === 'trade') {
-                  await me.visitTradeStrategy(strategy, symbol);
+                  //await me.visitTradeStrategy(strategy, symbol);
                 } else {
                   throw new Error(`Invalid strategy type${type.name}`);
                 }
